9ef6183bad80840d6cd284ce77d04661f7f81544
Antonio-Pocomento <pocomento.antonio@gmail.com>
2026-09-08 10:48:56 +0200

    Initial Commit

---
+++ backend/src/main/java/com/bugboard26/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.bugboard26.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Backend attivo!";
+    }
+}
